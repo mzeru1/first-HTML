02549d159fd6e3573517b08d9bf289a8bed85e8c
Meron Zeru <sologolom@gmail.com>
2019-05-08 16:38:41 -0400

    first HTML

---
+++ src/main/java/com/example/demo/Excercise1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excercise1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Excercise1Application.class, args);
+	}
+
+}
