42c372efdbe5df8a886be6580ad62f3c70f1d975
Meron Zeru <sologolom@gmail.com>
2019-05-09 15:43:41 -0400

    bootstrap

--- src/main/java/com/example/demo/Excercise1Application.java
+++ src/main/java/com/example/demo/Excercise1Application.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Excercise1Application {
-
 	public static void main(String[] args) {
-
-		SpringApplication.run(Excercise1Application.class, args);
+	    SpringApplication.run(Excercise1Application.class, args);
 	}
 
 }
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,12 +6,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String homepage() {
-        return "index";
-    }
     @RequestMapping("/Crabcake")
+    public String homepage() {
+        return "crabcake";
+    }
+    @RequestMapping("/recipe")
     public String crapcape () {
-        return "Crabcake";
+        return "recipe";
+    }
+    @RequestMapping("/recipe1")
+    public String recepie (){
+        return "recipe1";
+    }
+    @RequestMapping("/recipe2")
+    public String recipe2(){
+        return "recipe2";
+    }
+    @RequestMapping("/recipe3")
+    public String recipe3 (){
+        return "recipe3";
+    }
+    @RequestMapping("/recipe4")
+    public String recipe4 (){
+        return "recipe4";
+    }
+    @RequestMapping("/boodstrap")
+    public String bootstrap(){
+        return "bootstrap";
     }
 }
